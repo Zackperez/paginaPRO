a9ae5638744b6598bf92f9f34a3ad6d56a90aee5
Zacks <53922473+Zackperez@users.noreply.github.com>
2020-11-09 02:24:58 -0500

    Alertas sobre inputs
    Bugs cuando se da en el boton register varias veces, el tiempo no cuadra

--- js/register.js
+++ js/register.js
@@ -1,30 +1,56 @@
+// VARIABLES A USAR 	
+	var nombreModal = document.getElementById('nombre-modal');
+	var apellidoModal = document.getElementById('apellido-modal');
+	var correoModal = document.getElementById('email-modal');
+	var contraModal = document.getElementById('contrasena-modal');
+	var confirmContraModal = document.getElementById('contra-confirm-modal');
+	var textos  = [nombreModal.value, apellidoModal.value, correoModal.value, contraModal.value, confirmContraModal.value]; //Toma lo escrito de cada campo
+
+	var textos2 = [nombreModal, apellidoModal, correoModal, contraModal, confirmContraModal]; //Toma el campo HTML
+
+	console.log(nombreModal.className);
+
+
 document.getElementById('button-Modal').onclick = function(){
-	let nombreModal = document.getElementById('nombre-modal');
-	let apellidoModal = document.getElementById('apellido-modal');
-	let correoModal = document.getElementById('email-modal');
-	let contraModal = document.getElementById('contrasena-modal');
-	let confirmContraModal = document.getElementById('contra-confirm-modal');
 	var errorVerificacion = document.getElementById('mensajeVerificacion');
 
-	var textos  = [nombreModal.value, apellidoModal.value, correoModal.value, contraModal.value, confirmContraModal.value];
-	var textos2 = [nombreModal, apellidoModal, correoModal, contraModal, confirmContraModal];
-
+	//Para VALORES(variable creada por FOR) de textos(ARRAY QUE TOMA VALOR DE CAMPOS)
+	
 	for (var valor of textos) {
 		if  (valor.length == 0) {
+			//Otro for para modificar la clase de los INPUT de BOOTSTRAP -Los coloca de color rojo
+
 			for (elemento of textos2){
 				elemento.className = "form-control is-invalid";
-			}
+				}
 			errorVerificacion.innerHTML = '<h5 class = "mensajeCSS"> Faltan campos por completar</h5>';
-			tiempoEspera(errorVerificacion);
+			timer(errorVerificacion);
+
 			}
+
 		}
+
+	setInterval(function(){
+		nombreModal.className = "form-control";
+		apellidoModal.className = "form-control";
+		correoModal.className = "form-control";
+		contraModal.className = "form-control";
+		confirmContraModal.className = "form-control";
+	},3000);
+
 	}
 
-var error;
+var error; //variable que va a almacenar lo que se manda a la funcion
 
-function tiempoEspera(error){
+timer = function tiempoEspera(error){
 	setTimeout(function(){error.innerHTML = '<h5></h5>';},3000);
+	//			Agrega el HTML creado desde javascript || milisegundos 3000(3segundos)
 }
 
-
+function restaurarInput(textos2){
+	
+	for (var i = 0; i<textos2; i++){
+		textos2[i].className = "form-control";
+	}
+}
 
